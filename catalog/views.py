47262cbb88b98db4f7b9d08df4fda183c67e714b
taizuon <thaiduongabcdef@gmail.com>
2025-07-14 11:46:52 +0700

    use session to show num times user visit
    add comment to explain session usage

--- catalog/views.py
+++ catalog/views.py
@@ -22,11 +22,17 @@ def index(request):
     # Count of all authors
     num_authors = Author.objects.count()  
 
+    # Get the current number of visits from the session (default to 1 if not set),
+    # then increment and save it back to track how many times the user has visited the page.
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
